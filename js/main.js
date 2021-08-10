b5fa75f1f254c35f838545108fac6110f1e38213
Troy Dorman <troydorman@gmail.com>
2021-08-10 12:56:29 -0600

    created showTime function to continually update the time displayed on the screen to match the users real world time

--- js/main.js
+++ js/main.js
@@ -4,5 +4,31 @@ const time = document.getElementById("time"),
   name = document.getElementById("name"),
   focus = document.getElementById("focus");
 
-
 // show time
+function showTime() {
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+
+  // set am or pm
+  const amPm = hour >= 12 ? "PM" : "AM";
+
+  // 12 hour format
+  hour = hour % 12 || 12;
+
+  // output time
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    sec
+  )}`;
+
+  setTimeout(showTime, 1000);
+}
+
+// add zeros
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+// run
+showTime();
